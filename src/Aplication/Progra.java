08827e2c21ff3384c8b5681edaac292d42ce035d
Pyetro <pyetroribeirosantacruz@gmail.com>
2025-06-17 11:00:40 -0300

    Seller entity class

--- src/Aplication/Progra.java
+++ src/Aplication/Progra.java
@@ -1,9 +1,15 @@
 package Aplication;
 
+import java.util.Date;
+
 import Model.entities.Department;
+import Model.entities.Seller;
 
 public class Progra {
     public static void main(String[] args) {
         Department  obj = new Department(1, "Books");
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        System.out.println(obj);
+        System.out.println(seller);
     }
 }
